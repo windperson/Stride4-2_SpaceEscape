bc0f7e1cf1997c3476ce305461e7fc3147b7b2cf
Isak Pao <windperson@gmail.com>
2024-01-05 15:55:32 +0800

    Buggy: will crash when open first time

--- SpaceEscape.Android/SpaceEscapeActivity.cs
+++ SpaceEscape.Android/SpaceEscapeActivity.cs
@@ -1,20 +1,39 @@
 using Android.App;
 using Android.OS;
 using Android.Content.PM;
+using Android;
 
 using Stride.Engine;
 using Stride.Starter;
 
 namespace SpaceEscape
 {
-    [Activity(MainLauncher = true, 
-              Icon = "@mipmap/gameicon", 
+    [Activity(MainLauncher = true,
+              Icon = "@mipmap/gameicon",
               ScreenOrientation = ScreenOrientation.Portrait,
               Theme = "@android:style/Theme.NoTitleBar",
               ConfigurationChanges = ConfigChanges.UiMode | ConfigChanges.Orientation | ConfigChanges.KeyboardHidden | ConfigChanges.ScreenSize)]
     public class SpaceEscapeActivity : StrideActivity
     {
         protected Game Game;
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            // Check if the BLUETOOTH_CONNECT permission is granted
+            if (CheckSelfPermission(Manifest.Permission.BluetoothConnect) != Permission.Granted)
+            {
+                RequestPermissions(new string[] { Manifest.Permission.BluetoothConnect }, 0);
+            }
+
+            // ENSURE YOU DO PERMISSION BEFORE THIS LINE
+
+            base.OnCreate(bundle);
+        }
 
         protected override void OnRun()
         {
